44a0c8776e58a06c7e94ce43e5a80b764659d2e6
Jorge Contreras <jorge.contreras789@gmail.com>
2020-05-03 11:38:22 -0400

    Abstracted out logic for next getInitialProps to build-client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
@@ -6,20 +7,9 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    const { data } = await axios.get(
-      'http://ingress-nginx.ingress-nginx.svc.cluter.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
-
-    return data;
-  } else {
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser');
+  return data;
 };
 
 export default LandingPage;
