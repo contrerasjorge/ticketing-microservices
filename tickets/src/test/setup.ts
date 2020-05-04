1f216722d5099719ea720de80e22d943cdf66b2e
Jorge Contreras <jorge.contreras789@gmail.com>
2020-05-04 15:05:00 -0400

    Added tests and route handler for update ticket route

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,63 +1,63 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
-import jwt from 'jsonwebtoken';
-import { app } from '../app';
-import request from 'supertest';
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import { app } from "../app";
+import request from "supertest";
 
 declare global {
-  namespace NodeJS {
-    interface Global {
-      signin(): string[];
+    namespace NodeJS {
+        interface Global {
+            signin(): string[];
+        }
     }
-  }
 }
 
 let mongo: any;
 
 beforeAll(async () => {
-  process.env.JWT_KEY = 'atasdfasfsdf';
+    process.env.JWT_KEY = "atasdfasfsdf";
 
-  mongo = new MongoMemoryServer();
-  const mongoUri = await mongo.getUri();
+    mongo = new MongoMemoryServer();
+    const mongoUri = await mongo.getUri();
 
-  await mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+    await mongoose.connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 });
 
 beforeEach(async () => {
-  const collections = await mongoose.connection.db.collections();
+    const collections = await mongoose.connection.db.collections();
 
-  for (let collection of collections) {
-    await collection.deleteMany({});
-  }
+    for (let collection of collections) {
+        await collection.deleteMany({});
+    }
 });
 
 afterAll(async () => {
-  await mongo.stop();
-  await mongoose.connection.close();
+    await mongo.stop();
+    await mongoose.connection.close();
 });
 
 global.signin = () => {
-  // build a JWT payload {id, email}
-  const payload = {
-    id: '1453564587876',
-    email: 'test@test.com',
-  };
+    // build a JWT payload {id, email}
+    const payload = {
+        id: new mongoose.Types.ObjectId().toHexString(),
+        email: "test@test.com"
+    };
 
-  // Create the JWT
-  const token = jwt.sign(payload, process.env.JWT_KEY!);
+    // Create the JWT
+    const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  // Build session object {jwt: MY_JWT}
-  const session = { jwt: token };
+    // Build session object {jwt: MY_JWT}
+    const session = { jwt: token };
 
-  // Turn that session into JSON
-  const sessionJSON = JSON.stringify(session);
+    // Turn that session into JSON
+    const sessionJSON = JSON.stringify(session);
 
-  // Take JSON and encode it as base64
-  const base64 = Buffer.from(sessionJSON).toString('base64');
+    // Take JSON and encode it as base64
+    const base64 = Buffer.from(sessionJSON).toString("base64");
 
-  // Return a string that's the cookie with the encoded data
-  return [`express:sess=${base64}`];
+    // Return a string that's the cookie with the encoded data
+    return [`express:sess=${base64}`];
 };
