0a12f72c6a49b84dcaffdc70aeab5c65ce212308
Jorge Contreras <jorge.contreras789@gmail.com>
2020-05-08 09:16:05 -0400

    created a nats-wrapper and updated nats-depl.yaml

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,29 +1,31 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-import { app } from './app';
+import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined');
+    throw new Error("JWT_KEY must be defined");
   }
 
   if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined');
+    throw new Error("MONGO_URI must be defined");
   }
 
   try {
+    await natsWrapper.connect("ticketing", "dfsdfk", "http://nats-srv:4222");
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
+      useCreateIndex: true
     });
-    console.log('Connected to mongodb 😎');
+    console.log("Connected to mongodb 😎");
   } catch (err) {
     console.error(err);
   }
 
   app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!');
+    console.log("Listening on port 3000!!!!");
   });
 };
 
