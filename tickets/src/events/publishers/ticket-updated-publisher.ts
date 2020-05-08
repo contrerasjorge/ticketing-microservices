60d2e8b119df40c005b313960f0824d31dd480fe
Jorge Contreras <jorge.contreras789@gmail.com>
2020-05-08 09:34:36 -0400

    created a publisher for ticket update

---
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketUpdatedEvent } from "@jctickets/common";
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
