d7c4e4fda44b4afe3aeedd77c57bcb4cedfe391d
Jorge Contreras <jorge.contreras789@gmail.com>
2020-05-08 09:28:47 -0400

    added a graceful shutdown for nats on index.js

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,7 +2,8 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@jctickets/common";
 import { Ticket } from "../models/ticket";
-import {TicketCreatedPublisher} from "../events/publishers/ticket-created-publisher";
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -25,12 +26,12 @@ router.post(
       userId: req.currentUser!.id
     });
     await ticket.save();
-    await new TicketCreatedPublisher(client).publish({
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
       userId: ticket.userId
-    })
+    });
 
     res.status(201).send(ticket);
   }
