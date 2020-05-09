548036feadc6bb29792a530ab185463f00637ed5
Jorge Contreras <jorge.contreras789@gmail.com>
2020-05-09 10:53:57 -0400

    updated tickets test by creating a __mocks__ directory to account for NATS

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,77 +1,77 @@
-import request from 'supertest';
-import { app } from '../../app';
-import { Ticket } from '../../models/ticket';
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
 
-it('has a route handler listening to /api/tickets for post requests', async () => {
-  const response = await request(app).post('/api/tickets').send({});
+it("has a route handler listening to /api/tickets for post requests", async () => {
+  const response = await request(app).post("/api/tickets").send({});
 
   expect(response.status).not.toEqual(404);
 });
 
-it('can only be accessed if the user is signed in', async () => {
-  await request(app).post('/api/tickets').send({}).expect(401);
+it("can only be accessed if the user is signed in", async () => {
+  await request(app).post("/api/tickets").send({}).expect(401);
 });
 
-it('returns a status other than 401 if the user is signed in', async () => {
+it("returns a status other than 401 if the user is signed in", async () => {
   const response = await request(app)
-    .post('/api/tickets')
-    .set('Cookie', global.signin())
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
     .send({});
 
   expect(response.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid title is provided', async () => {
+it("returns an error if an invalid title is provided", async () => {
   await request(app)
-    .post('/api/tickets')
-    .set('Cookie', global.signin())
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
     .send({
-      title: '',
-      price: 10,
+      title: "",
+      price: 10
     })
     .expect(400);
 
   await request(app)
-    .post('/api/tickets')
-    .set('Cookie', global.signin())
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
     .send({
-      price: 10,
-    })
-    .expect(400);
-});
-
-it('returns an error if an invalid price is provided', async () => {
-  await request(app)
-    .post('/api/tickets')
-    .set('Cookie', global.signin())
-    .send({
-      title: '325436',
-      price: -10,
-    })
-    .expect(400);
-
-  await request(app)
-    .post('/api/tickets')
-    .set('Cookie', global.signin())
-    .send({
-      title: '43524362',
+      price: 10
     })
     .expect(400);
 });
 
-it('creates a ticket with valid inputs', async () => {
+it("returns an error if an invalid price is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "325436",
+      price: -10
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "43524362"
+    })
+    .expect(400);
+});
+
+it("creates a ticket with valid inputs", async () => {
   let tickets = await Ticket.find({});
 
   expect(tickets.length).toEqual(0);
 
-  const title = 'asdgweer4';
+  const title = "asdgweer4";
 
   await request(app)
-    .post('/api/tickets')
-    .set('Cookie', global.signin())
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
     .send({
       title,
-      price: 34,
+      price: 34
     })
     .expect(201);
 
