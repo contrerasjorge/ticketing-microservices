e5d05d895d9258b80eb1b99b0e038bd895f06c8c
Jorge Contreras <jorge.contreras789@gmail.com>
2020-05-10 10:23:36 -0400

    initial implementation of orders service routes

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -2,29 +2,29 @@ import express from "express";
 import "express-async-errors";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@jctickets/common";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
+import { deleteOrderRouter } from "./routes/delete";
+import { indexOrderRouter } from "./routes/index";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
 
 const app = express();
 app.set("trust proxy", true);
 app.use(express.json());
 app.use(
-    cookieSession({
-        signed: false,
-        secure: process.env.NODE_ENV !== "test"
-    })
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test"
+  })
 );
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
 
 app.all("*", async (req, res) => {
-    throw new NotFoundError();
+  throw new NotFoundError();
 });
 
 app.use(errorHandler);
