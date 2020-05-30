8ff5074c0aec0873afe9024b3c018bceec59e084
Jorge Contreras <jorge.contreras789@gmail.com>
2020-05-30 18:24:26 -0400

    payments service, created route handler for validating order payment and corresponding tests

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -4,7 +4,9 @@ import {
   requireAuth,
   validateRequest,
   BadRequestError,
-  NotFoundError
+  NotFoundError,
+  NotAuthorizedError,
+  OrderStatus
 } from "@jctickets/common";
 import { Order } from "../models/order";
 
@@ -13,8 +15,25 @@ const router = express.Router();
 router.post(
   "/api/payments",
   requireAuth,
-  [body("token").not().isEmpty, body("orderId").not().isEmpty()],
-  (req: Request, res: Response) => {
+  [body("token").not().isEmpty(), body("orderId").not().isEmpty()],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError("Cannot pay for a cancelled order");
+    }
+
     res.send({ success: true });
   }
 );
