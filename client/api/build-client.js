4ae2ffcd2233c52eec574c6b2364405fb8f1ec2b
Jorge Contreras <jorge.contreras789@gmail.com>
2020-05-31 19:34:03 -0400

    updated cookieSession

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,6 @@ export default ({ req }) => {
     return axios.create({
       baseURL:
         'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-
       headers: req.headers,
     });
   } else {
--- orders/src/app.ts
+++ orders/src/app.ts
@@ -1,19 +1,20 @@
-import express from "express";
-import "express-async-errors";
-import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError, currentUser } from "@jctickets/common";
-import { deleteOrderRouter } from "./routes/delete";
-import { indexOrderRouter } from "./routes/index";
-import { newOrderRouter } from "./routes/new";
-import { showOrderRouter } from "./routes/show";
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@jctickets/common';
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes/index';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
 
 const app = express();
-app.set("trust proxy", true);
+app.set('trust proxy', true);
 app.use(express.json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== "test"
+    // secure: process.env.NODE_ENV !== "test"
+    secure: false,
   })
 );
 app.use(currentUser);
@@ -23,7 +24,7 @@ app.use(indexOrderRouter);
 app.use(newOrderRouter);
 app.use(showOrderRouter);
 
-app.all("*", async (req, res) => {
+app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 
